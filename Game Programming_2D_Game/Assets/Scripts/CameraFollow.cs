814cf565e1fbfee65c1d57d592ea5216c1d05c03
JessChris <j.christodoulou@outlook.com>
2022-01-15 23:57:56 +0200

    Fixed Camera
    Edited the camera offset script. It is now simpler and works by taking the position of the player and adding an offset to it.

--- Game Programming_2D_Game/Assets/Scripts/CameraFollow.cs
+++ Game Programming_2D_Game/Assets/Scripts/CameraFollow.cs
@@ -4,25 +4,13 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    private Transform playerTransform;
+    public Transform target;
+    public Vector3 offset;
 
-    void Start()
+
+    private void FixedUpdate()
     {
-        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        transform.position = target.position+offset;
     }
 
-    
-    void LateUpdate()
-    {
-        //current position of the camera (temp)
-        Vector3 temp = playerTransform.position;
-
-        //set cameras position x to be equal to players position
-        temp.x = playerTransform.position.x;
-        temp.y = playerTransform.position.y;
-
-
-        //set back the camera's temp position to the cam's current position
-        playerTransform.position = temp;
-    }
 }
